8ecc1ea30a07de4ebfa220e05f5b316e0107da20
Diego Viana <dsantos@inf.ufsm.br>
2025-11-06 18:47:39 -0300

    Update diet-data.ts

--- diet-app-frontend/src/types/diet-data.ts
+++ diet-app-frontend/src/types/diet-data.ts
@@ -1,10 +1,11 @@
 import z from 'zod'
 
 export const DietDataSchema = z.object({
+  name: z.string().min(2).optional(),
   weight: z.number().positive(),
   height: z.number().positive(),
   age: z.number().positive().int(),
-  activity_level: z.enum(['sedentario', '2x_semana', '4x_semana']),
+  activityLevel: z.enum(['sedentario', '2x_semana', '4x_semana']),
   genre: z.enum(['masculino', 'feminino', 'outro']),
   goal: z.enum(['perda_de_peso', 'hipertrofia', 'manter_massa_muscular']),
 })
