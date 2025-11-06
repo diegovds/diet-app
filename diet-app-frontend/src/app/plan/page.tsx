6af853a74da2ba6b42a4ccc0021ffc7558e15eb5
Diego Viana <dsantos@inf.ufsm.br>
2025-11-06 16:05:06 -0300

    Update page.tsx

--- diet-app-frontend/src/app/plan/page.tsx
+++ diet-app-frontend/src/app/plan/page.tsx
@@ -1,9 +1,15 @@
+import { getAuthState } from '@/actions/get-auth-state'
+import { redirect } from 'next/navigation'
 import { DietForm } from '../_components/diet-form'
 
-export default function PlanPage() {
+export default async function PlanPage() {
+  const { token } = await getAuthState()
+
+  if (!token) return redirect('/')
+
   return (
     <div className="flex w-full items-center justify-center">
-      <DietForm />
+      <DietForm token={token} />
     </div>
   )
 }
