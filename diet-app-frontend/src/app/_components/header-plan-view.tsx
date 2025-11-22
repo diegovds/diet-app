afc8b6691e1df5beb5707dd71052045cf127d754
Diego Viana <dsantos@inf.ufsm.br>
2025-11-21 22:42:52 -0300

    Create header-plan-view.tsx

---
+++ diet-app-frontend/src/app/_components/header-plan-view.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { clearAuthCookie } from '@/actions/clear-auth-cookie'
+import { Button } from '@/components/ui/button'
+import { useAuthStore } from '@/store/auth'
+import { useRouter } from 'next/navigation'
+
+interface PlanViewProps {
+  username: string
+}
+
+export function HeaderPlanView({ username }: PlanViewProps) {
+  const router = useRouter()
+  const { clearToken } = useAuthStore()
+
+  const handleClick = async () => {
+    await clearAuthCookie()
+    clearToken()
+    router.refresh()
+  }
+
+  return (
+    <div className="flex justify-between">
+      <h1>Olá, {username}</h1>
+      <Button onClick={handleClick}>Sair</Button>
+    </div>
+  )
+}
