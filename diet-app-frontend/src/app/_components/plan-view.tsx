cda78666032065ba0d17172fc13b67513bd2c51d
Diego Viana <dsantos@inf.ufsm.br>
2025-11-21 22:19:13 -0300

    Create plan-view.tsx

---
+++ diet-app-frontend/src/app/_components/plan-view.tsx
@@ -0,0 +1,24 @@
+import ReactMarkdown from 'react-markdown'
+
+interface PlanViewProps {
+  plan: string
+}
+
+export function PlanView({ plan }: PlanViewProps) {
+  return (
+    <div className="rounded-lg bg-gray-100 p-4 shadow-md">
+      <ReactMarkdown
+        components={{
+          h2: (props) => (
+            <h2 className="my-1 text-xl font-bold text-green-600" {...props} />
+          ),
+          h1: (props) => (
+            <h1 className="mb-1 text-2xl font-bold text-zinc-900" {...props} />
+          ),
+        }}
+      >
+        {plan}
+      </ReactMarkdown>
+    </div>
+  )
+}
