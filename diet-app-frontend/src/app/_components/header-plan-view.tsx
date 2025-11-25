8e10e33a1f530384d3fd973c2c92fe755d3b7760
Diego Viana <dsantos@inf.ufsm.br>
2025-11-25 12:16:01 -0300

    Update header-plan-view.tsx

--- diet-app-frontend/src/app/_components/header-plan-view.tsx
+++ diet-app-frontend/src/app/_components/header-plan-view.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { useDeletePlanMutation } from '@/hooks/use-delete-plan-mutation'
 import { GetUser200 } from '@/http/api'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { DietForm } from './diet-form'
 
@@ -12,14 +14,36 @@ interface PlanViewProps {
 
 export function HeaderPlanView({ userData, token }: PlanViewProps) {
   const [updateData, setUpdateData] = useState(false)
+  const { mutate, isPending } = useDeletePlanMutation(token)
+  const router = useRouter()
+
+  const handleDeletePlan = () => {
+    mutate(undefined, {
+      onSuccess: () => {
+        router.refresh()
+      },
+    })
+  }
 
   return (
     <div className="">
       <div className="flex items-center justify-between gap-4">
         <h1>Olá, {userData.name}</h1>
-        <Button onClick={() => setUpdateData(!updateData)}>
-          {updateData ? 'Fechar' : 'Atualizar dados'}
-        </Button>
+        <div className="flex gap-4">
+          <Button
+            onClick={() => setUpdateData(!updateData)}
+            disabled={isPending}
+          >
+            {updateData ? 'Fechar' : 'Atualizar dados'}
+          </Button>
+          <Button
+            onClick={handleDeletePlan}
+            disabled={isPending}
+            variant="destructive"
+          >
+            Apagar dieta
+          </Button>
+        </div>
       </div>
       {updateData && (
         <div className="mt-4 flex justify-center">
