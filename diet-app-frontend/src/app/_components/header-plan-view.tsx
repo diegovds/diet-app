844da487f4e847bd4344e5cf454deba5ba98a33f
Diego Viana <dsantos@inf.ufsm.br>
2025-11-24 22:41:29 -0300

    Update header-plan-view.tsx

--- diet-app-frontend/src/app/_components/header-plan-view.tsx
+++ diet-app-frontend/src/app/_components/header-plan-view.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
 import { GetUser200 } from '@/http/api'
+import { useState } from 'react'
 import { DietForm } from './diet-form'
 
 interface PlanViewProps {
@@ -7,12 +11,21 @@ interface PlanViewProps {
 }
 
 export function HeaderPlanView({ userData, token }: PlanViewProps) {
+  const [updateData, setUpdateData] = useState(false)
+
   return (
-    <div className="space-y-5">
-      <h1>Olá, {userData.name}</h1>
-      <div className="flex justify-center">
-        <DietForm token={token} userData={userData} update />
+    <div className="">
+      <div className="flex items-center justify-between gap-4">
+        <h1>Olá, {userData.name}</h1>
+        <Button onClick={() => setUpdateData(!updateData)}>
+          {updateData ? 'Fechar' : 'Atualizar dados'}
+        </Button>
       </div>
+      {updateData && (
+        <div className="mt-4 flex justify-center">
+          <DietForm token={token} userData={userData} update />
+        </div>
+      )}
     </div>
   )
 }
