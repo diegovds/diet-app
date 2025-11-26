3bcafbd6b129cd2b949d2be25cdfd8489bc4a535
Diego Viana <dsantos@inf.ufsm.br>
2025-11-26 15:59:02 -0300

    Create diet.tsx

---
+++ diet-app-frontend/src/app/_components/diet.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { GetUser200 } from '@/http/api'
+import { useState } from 'react'
+import { HeaderPlanView } from './header-plan-view'
+import { PlanView } from './plan-view'
+
+interface DietProps {
+  token: string
+  userData: GetUser200
+}
+
+export function Diet({ token, userData }: DietProps) {
+  const [updateData, setUpdateData] = useState(false)
+
+  const openChangeCard = (open: boolean) => {
+    setUpdateData(open)
+  }
+
+  return (
+    <div className="w-full space-y-5">
+      <HeaderPlanView
+        token={token}
+        userData={userData}
+        openChangeCard={openChangeCard}
+      />
+      {userData.plan && !updateData && (
+        <PlanView plan={userData.plan.content} />
+      )}
+    </div>
+  )
+}
